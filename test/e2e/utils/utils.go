578785003f7969fcf70641d90c81efe2eaae8291
su-sudhir <shanmugams@google.com>
2026-05-22 15:30:14 +0530

    test: add dynamic volume E2E test for hyperdisk-balanced provisioning on HD-capable nodes

--- test/e2e/utils/utils.go
+++ test/e2e/utils/utils.go
@@ -69,6 +69,7 @@ func GCEClientAndDriverSetup(instance *remote.InstanceInfo, driverConfig DriverC
 		"--use-instance-api-to-list-volumes-published-nodes",
 		"--supports-dynamic-iops-provisioning=hyperdisk-balanced,hyperdisk-extreme",
 		"--supports-dynamic-throughput-provisioning=hyperdisk-balanced,hyperdisk-throughput,hyperdisk-ml",
+		"--dynamic-volumes=true",
 		"--allow-hdha-provisioning",
 		"--device-in-use-timeout=10s", // Set lower than the usual value to expedite tests
 		fmt.Sprintf("--fallback-requisite-zones=%s", strings.Join(driverConfig.Zones, ",")),
